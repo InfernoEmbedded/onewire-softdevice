40b3874d2c5db0ed3a6857f89e03344923bcbf2b
Alastair D'Silva <alastair@d-silva.org>
2017-06-08 19:50:45 +1000

    Refactor SwitchMaster to support On/Off zone pairs
    Signed-off-by: Alastair D'Silva <alastair@d-silva.org>

--- devices/Aircon_Zone_Controller/src/main.cpp
+++ devices/Aircon_Zone_Controller/src/main.cpp
@@ -17,7 +17,7 @@
 
 #include "mbed.h"
 
-#include <ACZoneController.h>
+#include <SwitchMaster.h>
 #include "hal_tick.h"
 #define WANT_TRACE 1
 #include "trace.h"
@@ -28,114 +28,77 @@ using namespace infernoembedded;
 Serial trace(USBTX, USBRX, "trace", 921600);
 #endif
 
-Timer timer;
-
-typedef struct Zone {
-	DigitalOut open;
-	DigitalOut close;
-	uint16_t timeout;
-	uint8_t desired;
-	uint8_t current;
-	Timer	lastActivated;
-} Zone;
-
-#define PF_8 ((PinName)0x57)
-
-class Listener : public ACZoneListener {
-protected:
-#define ZONE_COUNT	8
-	Zone _zones[ZONE_COUNT] = {
-					{PB_14, PB_15, 30, 0, 0},
-					{PC_6, PC_7, 30, 0, 0},
-					{PC_9, PA_9, 30, 0, 0},
-					{PA_11, PF_8, 30, 0, 0},
-					{PC_10, PC_12, 30, 0, 0},
-					{PB_4, PB_6, 30, 0, 0},
-					{PF_4, PF_5, 30, 0, 0},
-					{PB_12, PB_13, 30, 0, 0},
-	};
-
-public:
-	uint8_t countZones() {
-		return ZONE_COUNT;
+class MySwitchListener : public SwitchListener {
+	void setMode(uint8_t port, uint8_t channel, SwitchMode mode) {}
+	bool getState(uint8_t port, uint8_t channel) {
+		return false;
 	}
-
-	void setTimeout(uint8_t zone, uint8_t timeout) {
-		if (zone >= ZONE_COUNT) {
-			return;
-		}
-
-		_zones[zone].timeout = timeout * 1000; // Convert to ms
-	}
-
-	void setState(uint8_t zone, uint8_t state) {
-		if (zone >= ZONE_COUNT) {
-			return;
-		}
-
-		_zones[zone].desired = state;
-
-		bool shouldOpen = state > _zones[zone].current;
-
-		if (shouldOpen) {
-			_zones[zone].open = 1;
-			_zones[zone].close = 0;
-		} else {
-			_zones[zone].open = 0;
-			_zones[zone].close = 1;
-		}
-
-		_zones[zone].lastActivated.reset();
-		_zones[zone].lastActivated.start();
-
-		TRACE("Enabling damper for zone %d to %s", zone, shouldOpen ? "open" : "close");
-	}
-
-	/**
-	 * Disable any outputs that have timed out
-	 */
-	void poll() {
-		for (uint8_t zone = 0; zone < ZONE_COUNT; zone++) {
-			int delta = _zones[zone].desired - _zones[zone].current;
-
-			if (delta == 0) {
-				continue;
-			}
-
-			if (delta < 0) {
-				delta *= -1;
-			}
-
-			int elapsed = _zones[zone].lastActivated.read_ms();
-
-			if (elapsed > ((float)delta / 255) * _zones[zone].timeout) {
-				_zones[zone].lastActivated.stop();
-				_zones[zone].lastActivated.reset();
-				_zones[zone].open = 0;
-				_zones[zone].close = 0;
-				_zones[zone].current = _zones[zone].desired;
-
-				TRACE("Stopping actuator for zone %d after %d ms", zone, elapsed);
-			}
-		}
+	uint64_t getActivations(uint8_t port) {
+		return 0;
 	}
 };
 
-Listener listener;
+class myLEDListener : public LEDListener {
+	void setState(uint8_t port, uint8_t channel, bool on) {}
+	uint64_t getState(uint8_t port) {return 0;}
+};
+
+#define PF_8 ((PinName)0x57)
+
+class myRelayListener : public RelayListener {
+protected:
+#define ZONE_COUNT	8
+	DigitalOut _channels[ZONE_COUNT][2] = {
+					{PB_14, PB_15},
+					{PC_6, PC_7},
+					{PC_9, PA_9},
+					{PA_11, PF_8},
+					{PC_10, PC_12},
+					{PB_4, PB_6},
+					{PF_4, PF_5},
+					{PB_12, PB_13},
+	};
+
+public:
+	void setState(uint8_t port, uint8_t channel, bool state) {
+		if (port >= ZONE_COUNT) {
+			return;
+		}
+
+		if (channel >= 2) {
+			return;
+		}
+
+		_channels[port][channel] = state ? 1 : 0;
+
+		TRACE("Setting %d:%d %s", port, channel, state ? "on" : "off");
+	}
+
+	bool getState(uint8_t port, uint8_t channel) {
+		return !!_channels[port][channel].read();
+	}
+};
+
+MySwitchListener switchListener;
+myLEDListener ledListener;
+myRelayListener relayListener;
 
 OneWireAddress address;
-ACZoneController dev(PB_0, address, listener);
+SwitchMaster<0, 0, 0, 0, 8, 2> dev(PB_0, address, switchListener, ledListener, relayListener);
 
 
 int main() {
 	TRACE("AC Zone Controller Online, SystemCoreClock = %ld Hz", SystemCoreClock);
-	dev.getAddress(address); // populate the family & CRC
 
+	for (uint8_t port = 0; port < ZONE_COUNT; port++) {
+		dev.setRelayMode(port, 0, RelayMode::OPEN_CLOSE_PAIR);
+	}
+
+	dev.getAddress(address); // populate the family & CRC
 	TRACE("Address = %02x.%02x%02x%02x%02x%02x%02x.%02x", address.bytes[0], address.bytes[1], address.bytes[2],
 					address.bytes[3], address.bytes[4], address.bytes[5], address.bytes[6], address.bytes[7]);
 
     while (1) {
     	dev.poll();
-    	listener.poll();
     }
 }
--- libs/OneWireSlave/include/SwitchMaster.h
+++ libs/OneWireSlave/include/SwitchMaster.h
@@ -16,11 +16,14 @@
  */
 
 #ifndef SWITCHMASTER_H_
-#define SWITCHMASTER_H_
+#define RELAYMASTER_H_
 
 #define NDEBUG
 #include "mbed.h"
 #include <stdint.h>
+
+#define WANT_TRACE 1
+#include "trace.h"
 
 #include "SoftDevice.h"
 
@@ -35,18 +38,6 @@ enum class SwitchMode : uint8_t {
 
 class SwitchListener {
 public:
-	/**
-	 * Get the number of switch channels per port (max 64)
-	 * @return the number of channels
-	 */
-	virtual uint8_t countChannelsPerPort() =0;
-
-	/**
-	 * Get the number of switch ports
-	 * @return the number of ports
-	 */
-	virtual uint8_t countPorts() =0;
-
 	/**
 	 * Set a switch's mode
 	 * @param port the port of the switch
@@ -79,18 +70,6 @@ public:
 class LEDListener {
 public:
 	/**
-	 * Get the number of led channels per port
-	 * @return the number of channels
-	 */
-	virtual uint8_t countChannelsPerPort() =0;
-
-	/**
-	 * Get the number of led ports
-	 * @return the number of ports
-	 */
-	virtual uint8_t countPorts() =0;
-
-	/**
 	 * Set an LED's mode
 	 * @param port the port of the LED
 	 * @param channel the channel of the LED
@@ -99,37 +78,501 @@ public:
 	virtual void setState(uint8_t port, uint8_t channel, bool on) =0;
 
 	/**
-	 * Get the state of all LEDs on a port
-	 * @param port the port of the LED
-	 * @return the bitfield of all the LEDs on the port
+	 * Get a ports worth of LED states
+	 * @param port the port to get
 	 */
 	virtual uint64_t getState(uint8_t port) =0;
+};
+
+enum class RelayMode : uint8_t {
+	INDEPENDENT = 0, /**< Channel operates independently */
+	OPEN_CLOSE_PAIR = 1, /**< Channel operates as an open/close pair */
+};
+
+class RelayListener {
+public:
+	/**
+	 * Set the state for a channel
+	 * @param port the port the relay belongs to
+	 * @param channel The channel to operate on (must be even for an open/close pair)
+	 * @param on true to turn the channel on
+	 */
+	virtual void setState(uint8_t port, uint8_t channel, bool on) =0;
+
+	/**
+	 * Get the state of a relay
+	 * @param port the port to get
+	 * @param channel the channel to get
+	 */
+	virtual bool getState(uint8_t port, uint8_t channel) =0;
+};
+
+typedef struct RelayChannel {
+	uint8_t timeout;
+	RelayMode mode;
+	uint8_t current;
+	uint8_t desired;
+	Timer lastActivated;
+} RelayChannel;
+
+SoftDeviceCommandMetadata switchMasterMetadata[] = {
+				{"COUNT_SWITCHES",		0x01, 0, 0},
+				{"COUNT_LEDS",			0x02, 0, 0},
+				{"COUNT_RELAYS",		0x03, 0, 0},
+				{"SWITCH_TYPE",			0x04, 3, 0},
+				{"GET_SWITCH_STATE",	0x05, 0, 0},
+				{"GET_ACTIVATED_STATE",	0x06, 0, 0},
+				{"SET_LED",				0x07, 3, 0},
+				{"GET_LEDS",			0x08, 1, 0},
+				{"SET_RELAY_MODE",		0x09, 3, 0},
+				{"SET_RELAY_TIMEOUT",	0x0A, 3, 0},
+				{"SET_RELAY_STATE",		0x0B, 3, 0},
+				{"GET_RELAY_STATES",	0x0C, 0, 0},
+				{"GET_RELAY_MODES",		0x0D, 0, 0},
+				{"GET_RELAY_TIMEOUTS",	0x0E, 0, 0},
 };
 
 
 /**
  * SwitchMaster base class
  */
-class SwitchMaster : public SoftDevice {
+template<uint8_t switchPorts, uint8_t switchChannels, uint8_t ledPorts, uint8_t ledChannels, uint8_t relayPorts, uint8_t relayChannels>
+	class SwitchMaster : public SoftDevice {
 protected:
 	SwitchListener &_switchListener;
 	LEDListener &_ledListener;
-	uint64_t _activatedState;
-	uint8_t _currentPort;
+	RelayListener &_relayListener;
+	uint8_t _currentPort = 0;
+	uint8_t _currentChannel = 0;
+	RelayChannel _relayChannels[relayPorts][relayChannels];
+
+	void commandDespatch(uint8_t command) {
+		switch(command) {
+		case 0x01:
+			commandCountSwitches();
+			break;
+		case 0x02:
+			commandCountLEDs();
+			break;
+		case 0x03:
+			commandCountRelays();
+			break;
+		case 0x04:
+			commandSwitchType();
+			break;
+		case 0x05:
+			commandGetSwitchState();
+			break;
+		case 0x06:
+			commandGetActivatedState();
+			break;
+		case 0x07:
+			commandSetLED();
+			break;
+		case 0x08:
+			commandGetLED();
+			break;
+		case 0x09:
+			commandSetRelayMode();
+			break;
+		case 0x0A:
+			commandSetRelayTimeout();
+			break;
+		case 0x0B:
+			commandSetRelayState();
+			break;
+		case 0x0C:
+			commandGetRelayStates();
+			break;
+		case 0x0D:
+			commandGetRelayModes();
+			break;
+		case 0x0E:
+			commandGetRelayTimeouts();
+			break;
+		}
+	}
+
+	/**
+	 * Set the mode of a relay channel
+	 */
+	void commandSetRelayMode() {
+		uint8_t port = _data & 0xFF;
+		uint8_t channel = (_data >> 8) & 0xFF;
+		RelayMode mode = (RelayMode)((_data >> 16) & 0xFF);
+
+		setRelayMode(port, channel, mode);
+	}
+
+	/**
+	 * Set the timeout for an open/close relay channel
+	 */
+	void commandSetRelayTimeout() {
+		uint8_t port = _data & 0xFF;
+		uint8_t channel = (_data >> 8) & 0xFF;
+		uint8_t timeout = (_data >> 16) & 0xFF;
+
+		if (channel & 0x01) {
+			return; // Not an even channel
+		}
+
+		if (port >= relayPorts) {
+			return;
+		}
+
+		if (channel >= relayChannels) {
+			return;
+		}
+
+		_relayChannels[port][channel].timeout = timeout;
+
+		TRACE("Set timeout of %d:%d to %d", port, channel, timeout);
+	}
+
+	void commandSetRelayState() {
+		uint8_t port = _data & 0xFF;
+		uint8_t channel = (_data >> 8) & 0xFF;
+		uint8_t state = (_data >> 16) & 0xFF;
+
+		if (port >= relayPorts) {
+			return;
+		}
+
+		if (channel >= relayChannels) {
+			return;
+		}
+
+		if ((channel & 0x01) && _relayChannels[port][channel-1].mode == RelayMode::OPEN_CLOSE_PAIR) {
+			return; // Slave channel for an on/off pair
+		}
+
+		if (_relayChannels[port][channel].mode == RelayMode::INDEPENDENT) {
+			_relayListener.setState(port, channel, !!state);
+			return;
+		}
+
+		if (channel & 0x01) {
+			return; // Not an even channel
+		}
+
+		if (channel == relayChannels - 1) {
+			return; // Don't have a higher channel
+		}
+
+		// Mode is Open/Close pair
+		_relayChannels[port][channel].desired = state;
+
+		bool shouldOpen = state > _relayChannels[port][channel].current;
+
+		if (shouldOpen) {
+			_relayListener.setState(port, channel+1, false);
+			_relayListener.setState(port, channel, true);
+		} else {
+			_relayListener.setState(port, channel, false);
+			_relayListener.setState(port, channel+1, true);
+		}
+
+		_relayChannels[port][channel].lastActivated.reset();
+		_relayChannels[port][channel].lastActivated.start();
+	}
+
+	/**
+	 * Get all relay states
+	 * Writes:	N bytes, where each byte represents a channel on a port
+	 */
+	void commandGetRelayStates() {
+		_currentPort = 0;
+		_currentChannel = 0;
+
+		// continued in masterToReadCompleted()
+		masterToReadCompleted();
+	}
+
+	/**
+	 * Get all relay modes
+	 * Writes:	N bytes, where each byte represents a channel on a port
+	 */
+	void commandGetRelayModes() {
+		_currentPort = 0;
+		_currentChannel = 0;
+
+		// continued in masterToReadCompleted()
+		masterToReadCompleted();
+	}
+
+	/**
+	 * Get all relay timeouts
+	 * Writes:	N bytes, where each byte represents a channel on a port
+	 */
+	void commandGetRelayTimeouts() {
+		_currentPort = 0;
+		_currentChannel = 0;
+
+		// continued in masterToReadCompleted()
+		masterToReadCompleted();
+	}
+
+	/**
+	 * Count the numbers of switches on the device
+	 * Writes:
+	 * 	1 byte containing the number of ports
+	 * 	1 byte containing the number of switches
+	 */
+	void commandCountSwitches() {
+		masterToRead(switchPorts);
+		masterToRead(switchChannels);
+	}
+
+	/**
+	 * Count the numbers of LEDs on the device
+	 * Sends 1 byte containing the number of LEDs, maximum of 64
+	 */
+	void commandCountLEDs() {
+		masterToRead(ledPorts);
+		masterToRead(ledChannels);
+	}
+
+	/**
+	 * Count the numbers of LEDs on the device
+	 * Sends 1 byte containing the number of LEDs, maximum of 64
+	 */
+	void commandCountRelays() {
+		masterToRead(relayPorts);
+		masterToRead(relayChannels);
+	}
+
+	/**
+	 * Set the type of a switch
+	 * Reads: 	1 byte - switch port
+	 * 			1 byte - switch channel
+	 * 			1 byte - switch type (see SwitchMode)
+	 */
+	void commandSwitchType() {
+		uint8_t port = _data & 0xFF;
+		uint8_t channel = (_data >> 8) & 0xFF;
+		uint8_t mode = (_data >> 16) & 0xFF;
+
+		_switchListener.setMode(port, channel, (SwitchMode)mode);
+	}
+
+	/**
+	 * Get the state of a single switch
+	 * Reads: 	1 byte - switch port
+	 * 			1 byte - switch channel
+	 * Writes:  1 byte - 0 or 1, the current raw state of the switch
+	 */
+	void commandGetSwitchState() {
+		uint8_t port = _data & 0xFF;
+		uint8_t channel = (_data >> 8) & 0xFF;
+		masterToRead(_switchListener.getState(port, channel));
+	}
+
+	/**
+	 * Get the activated state of all switches
+	 * Writes:  N ports of M bytes, where N is the number of ports, and M being the minimum number of bytes to fit all channels
+	 */
+	void commandGetActivatedState() {
+		_currentPort = 0;
+
+		// continued in masterToReadCompleted()
+		masterToReadCompleted();
+	}
+
+	/**
+	 * Set 1 LED
+	 * Reads: 	1 byte - led port
+	 * 			1 byte - led channel
+	 * 			1 byte 0 or 1, turn the channel off or on
+	 */
+	void commandSetLED() {
+		uint8_t port = _data & 0xFF;
+		uint8_t channel = (_data >> 8) & 0xFF;
+		uint8_t state = (_data >> 16) & 0xFF;
+
+		if (port >= ledPorts) {
+			return;
+		}
+
+		if (channel >= ledChannels) {
+			return;
+		}
+
+		_ledListener.setState(port, channel, !!state);
+	}
+
+	/**
+	 * Get  full port of LEDs
+	 * Reads: 	1 byte - led port
+	 * Writes:	N bytes, representing a bitmask of the LEDs on the port (up to 8 bytes)
+	 */
+	void commandGetLED() {
+		uint8_t port = _data & 0xFF;
+
+		masterToRead(_ledListener.getState(port), (ledChannels + 7)/ 8);
+	}
+
+	/**
+	 * Notify the library that a switch has been activated
+	 */
+	void switchHasBeenActivated() {
+		setStatus(0x01 << 1);
+	}
+
+	void masterToReadCompleted() {
+		uint8_t byteCount;
+		uint8_t val;
+
+		switch (_currentCommand) {
+		case 0x06: /* GET_ACTIVATED_STATE */
+			if (_currentPort >= switchPorts) {
+				break;
+			}
+
+			byteCount = (switchChannels + 7) / 8;
+			masterToRead(_switchListener.getActivations(_currentPort), byteCount);
+
+			_currentPort++;
+
+			break;
+
+		case 0x0C: /* GET_RELAY_STATES */
+			if (_currentChannel >= relayChannels) {
+				_currentChannel = 0;
+				_currentPort++;
+			}
+
+			if (_currentPort >= relayPorts) {
+				break;
+			}
+
+			val = 0;
+			switch (_relayChannels[_currentPort][_currentChannel].mode) {
+			case RelayMode::INDEPENDENT:
+				val = _relayListener.getState(_currentPort, _currentChannel) ? 1 : 0;
+				break;
+			case RelayMode::OPEN_CLOSE_PAIR:
+				val = _relayChannels[_currentPort][_currentChannel].current;
+				break;
+			}
+			masterToRead(val);
+
+			_currentChannel++;
+
+			break;
+
+		case 0x0D: /* GET_RELAY_MODES */
+			if (_currentChannel >= relayChannels) {
+				_currentChannel = 0;
+				_currentPort++;
+			}
+
+			if (_currentPort >= relayPorts) {
+				break;
+			}
+
+			masterToRead((uint8_t)_relayChannels[_currentPort][_currentChannel].mode);
+
+			_currentChannel++;
+
+			break;
+
+		case 0x0E: /* GET_RELAY_TIMEOUTS */
+			if (_currentChannel >= relayChannels) {
+				_currentChannel = 0;
+				_currentPort++;
+			}
+
+			if (_currentPort >= relayPorts) {
+				break;
+			}
+
+			masterToRead((uint8_t)_relayChannels[_currentPort][_currentChannel].timeout);
+
+			_currentChannel++;
+
+			break;
+		}
+	}
 
 public:
-	SwitchMaster(PinName pin, OneWireAddress &address, SwitchListener &switchListener, LEDListener &ledListener);
-	void commandDespatch(uint8_t command);
-	void commandCountSwitches();
-	void commandCountLEDs();
-	void commandSwitchType();
-	void commandGetSwitchState();
-	void commandGetActivatedState();
-	void commandSetLEDs();
-	void commandSetLED();
-	void commandGetLED();
-	void switchHasBeenActivated();
-	void masterToReadCompleted();
+	/**
+	 * Constructor
+	 * @param pin	the pin the 1wire interface is on
+	 * @param address	the 1wire address of the device (only the lowest 48 bits are considered)
+	 * @param switchListener	a listener for switch commands
+	 * @param ledListener		a listener for LED commands
+	 */
+	SwitchMaster(PinName pin, OneWireAddress &address, SwitchListener &switchListener, LEDListener &ledListener, RelayListener &relayListener)
+		: SoftDevice(pin, address, SWITCH_MASTER, 1, switchMasterMetadata), _switchListener(switchListener), _ledListener(ledListener), _relayListener(relayListener) {
+		for (uint8_t relayPort = 0; relayPort < relayPorts; relayPort++) {
+			for (uint8_t relayChannel = 0; relayChannel < relayChannels; relayChannel++) {
+				_relayChannels[relayPort][relayChannel].timeout = 30;
+				_relayChannels[relayPort][relayChannel].mode = RelayMode::INDEPENDENT;
+				_relayChannels[relayPort][relayChannel].current = 0;
+				_relayChannels[relayPort][relayChannel].desired = 0;
+			}
+		}
+	}
+
+
+	/**
+	 * Set the mode of a relay channel
+	 * @param port the port of the relay
+	 * @param channel the channel of the relay
+	 * @param mode the desired mode
+	 */
+	void setRelayMode(uint8_t port, uint8_t channel, RelayMode mode) {
+		if (channel & 0x01) {
+			return; // Not an even channel
+		}
+
+		if (port >= relayPorts) {
+			return;
+		}
+
+		if (channel >= relayChannels) {
+			return;
+		}
+
+		_relayChannels[port][channel].mode = mode;
+	}
+
+	/**
+	 * Override the poll() method to deal with the timeouts
+	 */
+	void poll() {
+		OneWireSlave::poll();
+
+		for (uint8_t relayPort = 0; relayPort < relayPorts; relayPort++) {
+			for (uint8_t relayChannel = 0; relayChannel < relayChannels; relayChannel++) {
+				if (_relayChannels[relayPort][relayChannel].mode == RelayMode::INDEPENDENT) {
+					return;
+				}
+
+				int delta = _relayChannels[relayPort][relayChannel].desired - _relayChannels[relayPort][relayChannel].current;
+
+				if (delta == 0) {
+					continue;
+				}
+
+				if (delta < 0) {
+					delta *= -1;
+				}
+
+				int elapsed = _relayChannels[relayPort][relayChannel].lastActivated.read_ms();
+
+				if (elapsed >= ((float)delta * 1000 / 255) * _relayChannels[relayPort][relayChannel].timeout) {
+					_relayChannels[relayPort][relayChannel].lastActivated.stop();
+					_relayChannels[relayPort][relayChannel].lastActivated.reset();
+					_relayListener.setState(relayPort, relayChannel, false);
+					_relayListener.setState(relayPort, relayChannel+1, false);
+					_relayChannels[relayPort][relayChannel].current = _relayChannels[relayPort][relayChannel].desired;
+
+					TRACE("Stopping relays for %d:%d after %d ms", relayPort, relayChannel, elapsed);
+				}
+			}
+		}
+	}
 };
 
 } /* namespace infernoembedded */
