eb552c1f22c9f739f26fd899e5b3af56ea35e94e
Alastair D'Silva <alastair@d-silva.org>
2018-12-13 09:47:51 +1100

    Build mbed using the official ARM toolchain
    Signed-off-by: Alastair D'Silva <alastair@d-silva.org>

--- toolchain/build-arm.sh
+++ toolchain/build-arm.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-set -e 
-
 wget https://armkeil.blob.core.windows.net/developer/Files/downloads/gnu-rm/7-2018q2/gcc-arm-none-eabi-7-2018-q2-update-linux.tar.bz2
+
 tar jxf gcc-arm-none-eabi-7-2018-q2-update-linux.tar.bz2
 
