33bb7758878e23b0e29a0d466dea97c9ddd433bc
Alastair D'Silva <alastair@d-silva.org>
2018-12-21 10:09:08 +1100

    Remove old mbed dir
    Signed-off-by: Alastair D'Silva <alastair@d-silva.org>

--- mbed/build.bat
+++
@@ -1,5 +0,0 @@
-setlocal
-
-cd mbed-os
-c:\Python27\python.exe tools\build.py -c -v -m INFERNOEMBEDDED_SOFTDEVICE_BOOTLOADER -t GCC_ARM -j 4 --profile release
-c:\Python27\python.exe tools\build.py -c -v -m INFERNOEMBEDDED_SOFTDEVICE -t GCC_ARM -j 4 --profile release
